9d0f660b127ec5496cbcd3b43210191385912946
natalie <nataliediazrodriguez@gmail.com>
2019-08-17 21:00:28 -0400

    add seed dum

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,16 @@
 class Product < ApplicationRecord
-  # def is_discounted?
-  #   price < 10
-  # end
+  def is_discounted?
+    price < 10
+  end
 
-  # def tax
-  #   tax_rate = 0.09
-  #   price * tax_rate
-  # end
+  def tax
+    tax_rate = 0.09
+    price * tax_rate
+  end
 
-  # def total
-  #   price + tax
-  # end
+  def total
+    price + tax
+  end
 
   # def image_url
   #   if images.length > 0
