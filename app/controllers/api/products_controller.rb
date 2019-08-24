089dbf4b5dd8d8ff1dc82e526861981c6c0a37c4
Natalie-Rodriguez <nataliediazrodriguez@gmail.com>
2019-08-23 21:30:11 -0400

    adding sorting asc and desc by price

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,39 +1,62 @@
 class Api::ProductsController < ApplicationController
   
-  def index 
-    @products = Product.all
-    render "index.json.jb"
-  end 
+    # before_action :authenticate_admin, except: [:index, :show]
+  before_action :authenticate_admin, only: [:create, :update]
+  # before_action :authenticate_user, only: [:show]
 
-  def show 
-    @product = Product.find_by(id: params[:id])
-    render "show.json.jb"
+  def index
+    if params[:search]
+      @products = Product.where("name LIKE ?", "%#{params[:search]}%")
+    else
+      @products = Product.all
+    end
+
+    if params[:discount]
+      @products = @products.where("price < ?", 10)
+    end
+
+    if params[:sort] && params[:sort_order]
+      @products = @products.order(params[:sort] => params[:sort_order])
+    else
+      @products = @products.order(:id => :asc)
+    end
+    # @products = Product.all
+
+    # if params[:category]
+    #   category = Category.find_by(name: params[:category])
+    #   @products = category.products
+    # end
+
+    render 'index.json.jb'
   end
 
   def create
-    #make a brand new product
     @product = Product.new(
-      name: params[:input_name],
-      description: params[:input_description],
-      price: params[:input_price],
-      image_url: params[:input_image_url]
-
-      )
-  
-   if @product.save
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    if @product.save
       render 'show.json.jb'
     else
       render 'errors.json.jb', status: :unprocessible_entity
     end
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 
   def update
-    #find in database then update 
+    #find product and update
     @product = Product.find_by(id: params[:id])
     p "here is the price"
     p params[:price]
     if @product.update(name: params[:name] || @product.name,
          price: params[:price] || @product.price,
-         image_url: params[:image_url] || @product.image_url,
+         # image_url: params[:image_url] || @product.image_url,
          description: params[:description] || @product.description
         )
       render 'show.json.jb'
