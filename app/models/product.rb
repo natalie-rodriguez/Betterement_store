29c5da74e4bfa9116008ef3e1a552ba6f9bf9688
Natalie-Rodriguez <nataliediazrodriguez@gmail.com>
2019-08-24 15:05:51 -0400

    add belong to for products/suppliers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,18 @@
 class Product < ApplicationRecord
+  #active record method 
+  belongs_to :supplier 
+
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
   validates :description, length: {in: 10..500 }
+
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
 
   def is_discounted?
     price < 10
